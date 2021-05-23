969032151fc89d84cd9cf46324e7d00d15e9c4ec
Dudu Neto <dudumatosneto@gmail.com>
2021-05-23 20:42:02 -0300

    create auth middleware and install dotenv

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const jwt = require('jsonwebtoken');
 const postController = require('../controllers/post/post.controller');
 const multer = require('multer');
 
@@ -9,6 +9,28 @@ const router = express.Router();
 //= ===============================
 // Post routes
 //= ===============================
+router.get('/:audio_id',
+  postController.read
+)
+
+router.use((request, response, next) => {
+  console.log('Middleware para Posts ==> ', request.headers);
+  if(request.headers.authorization) {
+    const token = request.headers.authorization.split('Bearer ')[1];
+    jwt.verify(token, '123456', (err, decoded) => {
+      if(!err) {
+        console.log('Token Decoded => ', decoded);
+        request.userId = decoded.user.userId
+        next()
+      } else {
+        console.log('Token Error err => ', err)
+        return response.status(403).json({error: true, errorMessage: err.message})
+      }
+    })
+  } else {
+    response.status(403).json({error: true, errorMessage: 'Token não informado'})
+  }
+});
 
 router.post(
   '/',
@@ -16,8 +38,5 @@ router.post(
   postController.create,
 );
 
-router.get('/:audio_id',
-  postController.read
-)
 
 module.exports = router;
